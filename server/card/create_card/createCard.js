ec2dadaaeb67e99ced76ff9e4d15b433b1452b04
moon-GD <king4mun@gmail.com>
2023-01-19 15:23:17 +0900

    카드 순서 서버 반영 완료

--- server/card/create_card/createCard.js
+++ server/card/create_card/createCard.js
@@ -1,6 +1,10 @@
 import { cardListOnLocal } from "../../../public/js/store/store.js";
 import { FETCH_CARD_URL, POST_METHOD, POST_HEADER } from "../../../public/js/common/commonVariable.js";
-import { updateColumnLength } from "../../../public/js/component/column.js";
+import { 
+    getColumnNodeByStatus, getCardOrderByColumn, 
+    updateColumnLength 
+} from "../../../public/js/component/column.js";
+import { moveJSONDataOnOneColumn } from "../move_card/updateCardOrder.js";
 
 /** 로컬에 카드 JSON을 추가합니다. 
  * @param {number} status column status
@@ -35,8 +39,9 @@ function addCardJSON(status, title, content, cardID) {
         id: cardID
     }
 
+    moveJSONDataOnOneColumn(status);
+    addCardJSONOnLocal(status, newJSONData);åß
     addCardJSONOnServer(newJSONData);
-    addCardJSONOnLocal(status, newJSONData);
     updateColumnLength(status);
 }
 
