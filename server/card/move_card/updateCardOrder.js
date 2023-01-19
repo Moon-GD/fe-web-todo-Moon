4b12f6b4a9f190f814309b09b68a12db133163ac
moon-GD <king4mun@gmail.com>
2023-01-19 15:10:57 +0900

    updateCardOrder.js 주석 추가가

--- server/card/move_card/updateCardOrder.js
+++ server/card/move_card/updateCardOrder.js
@@ -3,10 +3,18 @@ import { CARD_ORDER, FETCH_STATUS_URL, PATCH_METHOD, PATCH_HEADER } from "../../
 import { getColumnNodeByStatus, getCardOrderByColumn, updateColumnLength } from "../../../public/js/component/column.js";
 import { pipe } from "../../../public/js/common/commonFunction.js";
 
-function changeCardOrderOnLocal(statusID, newCardOrder) {
-    statusListOnLocal[statusID][CARD_ORDER] = newCardOrder;
-}
+/**
+ * local에서 카드 순서 정보를 갱신합니다.
+ * @param {number} statusID column ID
+ * @param {Array} newCardOrder card order Array
+ */
+function changeCardOrderOnLocal(statusID, newCardOrder) { statusListOnLocal[statusID][CARD_ORDER] = newCardOrder; }
 
+/**
+ * server에서 카드 순서 정보를 갱신합니다.
+ * @param {number} statusID column ID
+ * @param {Array} newCardOrder card order Array
+ */
 function changeCardOrderOnServer(statusID, newCardOrder) {
     fetch(FETCH_STATUS_URL + "/" + statusID, {
         method: PATCH_METHOD,
@@ -15,11 +23,20 @@ function changeCardOrderOnServer(statusID, newCardOrder) {
     })
 }
 
+/**
+ * 카드 순서 정보를 갱신합니다.
+ * @param {number} statusID 
+ * @param {Array} newCardOrder 
+ */
 function changeCardOrder(statusID, newCardOrder) {
     changeCardOrderOnServer(statusID, newCardOrder);
     changeCardOrderOnLocal(statusID, newCardOrder);
 }
 
+/**
+ * column 내부 카드 순서를 갱신합니다.
+ * @param {number} status column status
+ */
 function moveJSONDataOnOneColumn(status) {
     const $cardOrder = pipe(
         getColumnNodeByStatus,
@@ -30,6 +47,11 @@ function moveJSONDataOnOneColumn(status) {
     updateColumnLength(status);
 }
 
+/**
+ * column 2개 내부의 카드 순서를 갱신합니다.
+ * @param {number} prevStatus 이전 column status
+ * @param {number} nextStatus 다음 column status
+ */
 function moveJSONDataOnTwoColumn(prevStatus, nextStatus) {
     moveJSONDataOnOneColumn(prevStatus);
     moveJSONDataOnOneColumn(nextStatus);
