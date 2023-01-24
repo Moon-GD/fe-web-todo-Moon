1e9df89ab7b3e7da1dc39a7ab516a26da2fd7c31
moon-GD <king4mun@gmail.com>
2023-01-24 15:31:43 +0900

    다크 모드 전구 토글 Css로 변경

--- public/js/common/darkMode.js
+++ public/js/common/darkMode.js
@@ -1,40 +1,26 @@
-import { querySelector, multiQuerySelector } from "../devUtils/querySelector.js";
-import { changeCSS } from "./commonFunction.js";
-import { DISPLAY, EVENT } from "./commonVariable.js";
+import { addEvent } from "./commonFunction.js";
+import { multiQuerySelector } from "../devUtils/querySelector.js";
 
-const [$body, bulbArea, darkBulb, lightBulb] = 
-    multiQuerySelector(["body", "#bulb-area", "#go-dark-mode", "#go-light-mode"]);
+const [$body, bulbArea] = multiQuerySelector(["body", "#bulb-area"]);
 
 /** card에 부여된 css를 초기화합니다. */
 function initializeCardCSS() {
-    const $cardList = document.querySelectorAll(".card-frame");
-    $cardList.forEach(($card) => { $card.style = "";})
+    const $cardArray = document.querySelectorAll(".card-frame");
+    $cardArray.forEach(($card) => $card.style = "");
 }
 
 /** 현재 dark mode 인지 여부를 반환합니다.
  * @returns {Boolean} true / false
  */
-
 DOMTokenList.prototype.includes = String.prototype.includes;
 const isDarkMode = () => $body.classList.includes("dark-mode")
 
-
 /** 전구 버튼에 다크 모드 토글 이벤트를 추가합니다. */
 function eventToBulbs() {
-    bulbArea.addEventListener(EVENT.CLICK, () => {
-        console.log(isDarkMode())
-        $body.classList.toggle("dark-mode");
-        initializeCardCSS()
-
-        if(isDarkMode()) {
-            changeCSS(lightBulb, "display", DISPLAY.BLOCK);
-            changeCSS(darkBulb, "display", DISPLAY.NONE);
-        }
-        else {
-            changeCSS(lightBulb, "display", DISPLAY.NONE);
-            changeCSS(darkBulb, "display", DISPLAY.BLOCK);
-        }
-    })
+    addEvent(bulbArea, [
+        () => $body.classList.toggle("dark-mode"),
+        () => initializeCardCSS()
+    ])
 }
 
 export { eventToBulbs, isDarkMode }
